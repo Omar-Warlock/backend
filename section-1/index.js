97ba24bb13bcb8cc727ce5a16b71fda63b3fbdcf
OMR <cartoon6676@gmail.com>
2026-03-31 17:03:29 +0200

    reading and writing in async approach

--- section-1/index.js
+++ section-1/index.js
@@ -1,5 +1,8 @@
 // file system module
 const fs = require("fs");
+
+// Blocking Synchronous way
+/* 
 // read a file
 const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textInput);
@@ -11,3 +14,24 @@ console.log("File written!");
 // use node command
 // const hello = "Hello World";
 // console.log(hello);
+
+*/
+
+// Non-blocking async way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log("\n" + data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has ben written 😁");
+      });
+    });
+  });
+});
+// callback hell
+console.log("Reading data...");
