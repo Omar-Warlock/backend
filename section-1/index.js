93c7945235ec58485899833063ba16d938939642
OMR <cartoon6676@gmail.com>
2026-03-31 18:18:06 +0200

    build a first simple web api

--- section-1/index.js
+++ section-1/index.js
@@ -1,6 +1,11 @@
 // file system module
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+///////////////////////////////////////
+///////////////////////////////////////
+// FILES
 // Blocking Synchronous way
 /* 
 // read a file
@@ -17,6 +22,7 @@ console.log("File written!");
 
 */
 
+/*
 // Non-blocking async way
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   if (err) {
@@ -35,3 +41,36 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 });
 // callback hell
 console.log("Reading data...");
+
+
+ */
+///////////////////////////////////////
+///////////////////////////////////////
+// SERVERS
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("<h1 >Home page</h1>");
+  } else if (pathName === "/products") {
+    res.end("this is the products");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1 style=`color-red`>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server runs ... \nListening to requests on port 8000");
+});
